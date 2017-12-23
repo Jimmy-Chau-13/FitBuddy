84a7837b945855e8a6dfff75326ab938639255ba
Jimmy <chaujimmy13@gmail.com>
2017-12-22 23:58:56 -0800

    added equals and hascode for quick look up time for hashset

--- src/main/java/app/friends/Friends.java
+++ src/main/java/app/friends/Friends.java
@@ -1,5 +1,7 @@
 package app.friends;
 
+import java.util.Objects;
+
 public class Friends {
 
     private String username;
@@ -10,5 +12,18 @@ public class Friends {
 
     public String getUsername() {
         return username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Friends friends = (Friends) o;
+        return Objects.equals(username, friends.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
     }
 }
