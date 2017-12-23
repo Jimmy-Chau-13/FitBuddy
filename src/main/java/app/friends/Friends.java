472b5d6c753c2d9cbc02392e19d6cab30482cde4
Jimmy <chaujimmy13@gmail.com>
2017-12-22 23:09:09 -0800

    Created Friends object, has one field:  String username

---
+++ src/main/java/app/friends/Friends.java
@@ -0,0 +1,14 @@
+package app.friends;
+
+public class Friends {
+
+    private String username;
+
+    public Friends(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
