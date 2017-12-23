5bd5030a61563cadf8a3a4474e0278280a186baf
Jimmy <chaujimmy13@gmail.com>
2017-12-22 22:57:02 -0800

    serveFriends includes username in response

--- src/main/java/app/friends/FriendsController.java
+++ src/main/java/app/friends/FriendsController.java
@@ -5,12 +5,17 @@ import app.util.Path;
 import spark.ModelAndView;
 import spark.*;
 
+import java.util.HashMap;
+
 public class FriendsController {
 
     public static ModelAndView serveFriends(Request req, Response res) {
         String userId = AuthController.checkSessionHasUser(req);
         if(userId != null && !userId.isEmpty()) {
-            return new ModelAndView(null, Path.Template.FRIENDS);
+            HashMap<String, Object> model = new HashMap<>();
+            String username = req.session(false).attribute(Path.Attribute.USERNAME).toString();
+            model.put("username", username );
+            return new ModelAndView(model, Path.Template.FRIENDS);
         }
         res.redirect(Path.Web.GET_INDEX_PAGE);
         return null;
