a69531fc13b24253422ea14c96ea3276e1e249d5
Jimmy <chaujimmy13@gmail.com>
2017-12-23 17:49:26 -0800

    fields in Friends are now json strings that represent hashsets

--- src/main/java/app/friends/Friends.java
+++ src/main/java/app/friends/Friends.java
@@ -1,29 +1,47 @@
 package app.friends;
 
+import com.google.gson.Gson;
+import org.mongodb.morphia.annotations.Embedded;
+
 import java.util.HashSet;
 
 
+@Embedded
 public class Friends {
 
-    private HashSet<String> current_friends;
-    private HashSet<String> pending_friends_added;
-    private HashSet<String> pending_friends_invitation;
+    // Json String of HashSets
+    private String current_friends;
+    private String pending_friends_added;
+    private String pending_friends_invitation;
 
     public Friends() {
-        this.current_friends = new HashSet<>();
-        this.pending_friends_added = new HashSet<>();
-        this.pending_friends_invitation = new HashSet<>();
+        Gson gson = new Gson();
+        this.current_friends = gson.toJson(new HashSet<String>());
+        this.pending_friends_added = gson.toJson(new HashSet<String>());
+        this.pending_friends_invitation = gson.toJson(new HashSet<String>());
     }
 
-    public HashSet<String> getCurrent_friends() {
+    public String getCurrent_friends() {
         return current_friends;
     }
 
-    public HashSet<String> getPending_friends_added() {
+    public void setCurrent_friends(String current_friends) {
+        this.current_friends = current_friends;
+    }
+
+    public String getPending_friends_added() {
         return pending_friends_added;
     }
 
-    public HashSet<String> getPending_friends_invitation() {
+    public void setPending_friends_added(String pending_friends_added) {
+        this.pending_friends_added = pending_friends_added;
+    }
+
+    public String getPending_friends_invitation() {
         return pending_friends_invitation;
+    }
+
+    public void setPending_friends_invitation(String pending_friends_invitation) {
+        this.pending_friends_invitation = pending_friends_invitation;
     }
 }
